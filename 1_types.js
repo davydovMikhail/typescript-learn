e9301e49ae6d646527c927120fdfc4a7553b2a0c
davydovMikhail <davydov27977@gmail.com>
2022-02-16 00:18:55 +0700

    2nd

--- 1_types.js
+++ 1_types.js
@@ -10,3 +10,19 @@ var words = ['Hello', 'world'];
 // тип данных Tuple - массив котррый состоит из разных типов данных 
 var contact = ['misha', 8999];
 // const contact: [number, string] = ['misha', 8999] так не канает
+// Any - тип благодаря которому можно совершать переприсваивания на другие типы
+var variable = 42;
+variable = 'new string';
+variable = [];
+//типы в функциях 
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Давыдов');
+// never - тип, который нужно указывать если функция возвращает ошибку и не заканчивает свое выполнение, либо когда она постонно что либо делает
+function throwError(message) {
+    throw new Error(message);
+}
+var login = 'admin';
+var id1 = 1234;
+var id2 = '1234';
--- 1_types.ts
+++ 1_types.ts
@@ -23,3 +23,34 @@ let variable: any = 42
 variable = 'new string'
 variable = []
 
+//типы в функциях 
+function sayMyName(name: string) : void { // void говорит о том что функция ничего не возвращает
+    console.log(name)
+}
+
+sayMyName('Давыдов')  
+
+// never - тип, который нужно указывать если функция возвращает ошибку и не заканчивает свое выполнение, либо когда она постонно что либо делает
+function throwError(message: string): never {
+    throw new Error(message)
+}
+
+// function infinite(): never { // функция никлгда не завершит свое выолнение поэтому never
+    // while (true) {
+    //не запускать, иниче все зависнит 
+    // }
+// }
+
+// Type -- можно создавать собственные типы
+
+type Login = string
+
+const login: Login = 'admin'
+
+type ID = string | number // -- ситуация когда значение может иметь два типа
+const id1: ID = 1234
+const id2: ID = '1234'
+// const id3: ID = true -- это работать не будет
+
+type SomeType = string | null | undefined
+
